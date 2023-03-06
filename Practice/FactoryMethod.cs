ea1fa51f8f3110da9845cec9601ab03dc8af598b
Kira3321 <mkonoval97@mail.ru>
2023-03-06 23:33:06 +0300

    learned and understood how the template works FactoryMethod

---
+++ Practice/FactoryMethod.cs
@@ -0,0 +1,43 @@
+﻿namespace Practice;
+
+public interface ICall
+{
+    void Calling();
+}
+
+public class MobilePhone: ICall
+{
+    public void Calling() => Console.WriteLine("Звоним через мобильный");
+}
+
+public class Telephone : ICall
+{
+    public void Calling() => Console.WriteLine("Звоним со стационарного телефона");
+}
+
+public abstract class PhoneCreator
+{
+    public abstract ICall FactoryMethod();
+}
+
+public class MobilephoneCreator : PhoneCreator
+{
+    public override ICall FactoryMethod() => new MobilePhone();
+}
+
+public class TelephoneCreator : PhoneCreator
+{
+    public override ICall FactoryMethod() => new Telephone();
+}
+
+public class Client
+{
+    private ICall _phone;
+
+    public Client(ICall phone)
+    {
+        _phone = phone;
+    }
+
+    public void Call() => _phone.Calling();
+}
--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Practice;
 
-var l = Logger.GetInstance();
+var mobcre = new MobilephoneCreator();
+var telcre = new TelephoneCreator();
 
-l.Log("Привет");
+var cl1 = new[] {new Client(mobcre.FactoryMethod()), new Client(telcre.FactoryMethod())};
+
+foreach (var v in cl1) v.Call();
