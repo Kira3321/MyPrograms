5e01b914023dddcdebb987da11575a00ecd8a130
Kira3321 <mkonoval97@mail.ru>
2023-03-07 19:47:14 +0300

    learned and understood how the template works Strategy

---
+++ Practice/TemplatesOOP/Strategy.cs
@@ -0,0 +1,34 @@
+﻿namespace Practice.TemplatesOOP;
+
+public interface IStrategy
+{
+    void Execute();
+}
+
+public class BubbleSort : IStrategy
+{
+    public void Execute() => Console.WriteLine("Сортировка пузырьком");
+}
+
+public class MergeSort : IStrategy
+{
+    public void Execute() => Console.WriteLine("Сортировка слиянием");
+}
+
+public class QuickSort : IStrategy
+{
+    public void Execute() => Console.WriteLine("Быстрая сортировка");
+}
+
+public class Context
+{
+    private IStrategy _strategy;
+
+    public Context(IStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void SetStrategy(IStrategy strategy) => _strategy = strategy;
+    public void Execute() => _strategy.Execute();
+}
