df45a115acb8e7b5c1a2240d429bc3dad9a9446e
Kira3321 <mkonoval97@mail.ru>
2023-03-07 21:27:49 +0300

    learned and understood how the template works Proxy

---
+++ Practice/TemplatesOOP/Structural/Proxy.cs
@@ -0,0 +1,67 @@
+﻿namespace Practice.TemplatesOOP.Structural;
+
+public class Data
+{
+    public int Id { get; set; }
+
+    public string Information { get; set; }
+}
+
+
+public class Dbset<T> : IDisposable
+{
+    public List<T> Base = new List<T>();
+    public void Dispose() => Console.WriteLine("Уничтожили объект Dbset");
+}
+
+public interface IDatabase : IDisposable
+{
+    Data? ReadData(int id);
+    void WriteData(Data data);
+}
+
+public class DataBase : IDatabase
+{
+    private Dbset<Data> _store;
+
+    public DataBase()
+    {
+        _store = new Dbset<Data>();
+    }
+
+    public Data? ReadData(int id) => _store.Base.FirstOrDefault(x => x.Id == id);
+
+    public void WriteData(Data data) => _store.Base.Add(data);
+
+    public void Dispose() => _store.Dispose();
+}
+
+public class ProxyDataBase : IDatabase
+{
+    private List<Data> _datas;
+    private DataBase _store;
+
+    public ProxyDataBase()
+    {
+        _datas = new List<Data>();
+    }
+
+    public Data? ReadData(int id)
+    {
+        var date = _datas.FirstOrDefault(x => x.Id == id);
+        if (date != null) return date;
+        if (_store == null) _store = new DataBase();
+        date = _store.ReadData(id);
+        _datas.Add(date);
+        return date;
+    }
+
+    public void WriteData(Data data)
+    {
+        if (_datas.Contains(data)) return;
+        var store = _store ?? new DataBase();
+        store.WriteData(data);
+    }
+
+    public void Dispose() => _store.Dispose();
+}
