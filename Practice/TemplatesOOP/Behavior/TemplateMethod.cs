e2a076c32db75bf27fba955bda8748ea6d1dfca4
Kira3321 <mkonoval97@mail.ru>
2023-03-08 22:38:28 +0300

    learned and understood how the template works Template Method

---
+++ Practice/TemplatesOOP/Behavior/TemplateMethod.cs
@@ -0,0 +1,116 @@
+﻿namespace Practice.TemplatesOOP.Behavior;
+
+public abstract class Delivery
+{
+    public void TemplateMethod()
+    {
+        Console.WriteLine(GetCost());
+        Console.WriteLine(GetDeliveryType());
+    }
+        
+    public abstract int GetCost();
+
+    public abstract string GetDeliveryType();
+
+    public abstract int UrgentDelivery();
+
+    public abstract int PerCent();
+
+    public abstract int Insurance();
+}
+
+public class AirDelivery : Delivery
+{
+    private int _cost;
+
+    public AirDelivery()
+    {
+        _cost = 150;
+    }
+
+    public override int GetCost() => _cost;
+
+    public override string GetDeliveryType() => this.GetType().ToString();
+
+    public override int UrgentDelivery()
+    {
+        _cost += (int)(_cost * 0.25);
+        return _cost;
+    }
+
+    public override int PerCent()
+    {
+        _cost -= (int)(_cost * 0.1);
+        return _cost;
+    }
+
+    public override int Insurance()
+    {
+        _cost -= (int)(_cost * 0.05);
+        return _cost;
+    }
+}
+
+public class SeaDelivery : Delivery
+{
+    private int _cost;
+
+    public SeaDelivery()
+    {
+        _cost = 120;
+    }
+
+    public override int GetCost() => _cost;
+
+    public override string GetDeliveryType() => this.GetType().ToString();
+
+    public override int UrgentDelivery()
+    {
+        _cost += (int)(_cost * 0.25);
+        return _cost;
+    }
+
+    public override int PerCent()
+    {
+        _cost -= (int)(_cost * 0.1);
+        return _cost;
+    }
+
+    public override int Insurance()
+    {
+        _cost -= (int)(_cost * 0.05);
+        return _cost;
+    }
+}
+
+public class GroundDelivery : Delivery
+{
+    private int _cost;
+
+    public GroundDelivery()
+    {
+        _cost = 100;
+    }
+
+    public override int GetCost() => _cost;
+
+    public override string GetDeliveryType() => this.GetType().ToString();
+
+    public override int UrgentDelivery()
+    {
+        _cost += (int)(_cost * 0.25);
+        return _cost;
+    }
+
+    public override int PerCent()
+    {
+        _cost -= (int)(_cost * 0.1);
+        return _cost;
+    }
+
+    public override int Insurance()
+    {
+        _cost -= (int)(_cost * 0.05);
+        return _cost;
+    }
+}
