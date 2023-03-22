556ed7066dd0610e9cd607084e4cb63a782dc8c3
Kira3321 <mkonoval97@mail.ru>
2023-03-22 22:10:54 +0300

    learned and understood how the template works Bridge

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,6 +1,5 @@
-﻿using System.Diagnostics;
-using System.Text.RegularExpressions;
-using Practice.MainScenario.Threads.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Practice;
 
@@ -8,12 +7,6 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
-        string input = "5-3EUY8OP6,  5-3EUTY8OP6! 5-3ECMKD65dick5-3EUY8OP655-3EUTY8OP6";
-        Regex regex = new Regex(@"(\d-[A-Z0-9]+[^\d-]{2})");
-        MatchCollection matches = regex.Matches(input);
-        foreach (Match match in matches)
-        {
-            Console.WriteLine(match.Value);
-        }
+        Console.WriteLine("нелло");
     }
 }
---
+++ Practice/TemplatesOOP/Structural/Bridge.cs
@@ -0,0 +1,51 @@
+﻿namespace Practice.TemplatesOOP.Structural;
+
+public abstract class Vehicle
+{
+    protected VehicleEngene _vehicleEngene;
+
+    public VehicleEngene VehicleEngene
+    {
+        set
+        {
+            _vehicleEngene = value;
+        }
+    }
+
+    public Vehicle(VehicleEngene vehicleEngene)
+    {
+        _vehicleEngene = vehicleEngene;
+    }
+
+    public void Work()
+    {
+        _vehicleEngene.Work();
+    }
+    
+}
+
+public class Car : Vehicle
+{
+    public Car(VehicleEngene vehicleEngene) : base(vehicleEngene)
+    {
+    }
+}
+
+public abstract class VehicleEngene
+{
+    public abstract void Work();
+}
+
+public class BioEngene : VehicleEngene
+{
+    public override void Work()
+    {
+    }
+}
+
+public class GasEngene : VehicleEngene
+{
+    public override void Work()
+    {
+    }
+}
